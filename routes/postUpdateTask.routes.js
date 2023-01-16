7ba96393b4115f68bb154a10fb0447f0d56bc67e
Dominguez <migueldominguezroman01@gmail.com>
2023-01-16 09:47:43 +0100

    16/01/2023 - Added Update rout

--- routes/postUpdateTask.routes.js
+++ routes/postUpdateTask.routes.js
@@ -7,8 +7,8 @@ router.post("/updateTask", async(req, res) =>{
 
     if((await task.find({id: userData.id})).length != 0){
         //console.log(userData)
-        console.log(await task.find({id_task: Number(userData.id_task)}))
-        await task.updateOne({id_task: Number(userData.id_task)}, {$set:{
+        console.log(await task.collection.find({id_task: Number(userData.id_task)}))
+        await task.collection.updateOne({id_task: Number(userData.id_task)}, {$set:{
             task: userData.task
         }})
         res.send("Ok")
