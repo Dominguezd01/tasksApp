3e3c19d4e5008b48ead32a9e7d3f0e7b2c9b1b8d
Dominguezd01 <migueldominguezroman01@gmail.com>
2023-01-16 21:03:38 +0100

    16/01/2023 - Update route fixed updated task model

--- models/tasks.js
+++ models/tasks.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 
 
 const task = new mongoose.Schema({
+    idTask:Number,
     taskContent: String
 })
 
--- routes/postUpdateTask.routes.js
+++ routes/postUpdateTask.routes.js
@@ -1,25 +1,24 @@
 const express= require("express")
 const router = express.Router()
 const task = require("../models/tasks")
+const { ObjectId } = require("mongodb")
 
 router.post("/updateTask", async(req, res) =>{
     const userData = await req.body
     
-    task.collection.findOne({id_task: userData.id_task})
+    task.collection.findOne({idTask: Number(userData.idTask)})
     .then(res => query(res))
     
     async function query(result){ 
         if(result != null){
-            await task.collection.updateOne({id_task: Number(userData.id_task)}, {$set:{
-                id_task: userData.id_task
+            await task.collection.updateOne({idTask:userData.idTask}, {$set:{
+                task: userData.task
             }})
             res.send("Ok")
         }else{
             res.send({status: "No se encontro ningún usuario asociado a ese identificador"})
         }
     }
-    
-    
 })
 
 
