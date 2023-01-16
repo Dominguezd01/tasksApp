96831e69e70b3723be0b46772afc58032419d9d3
Dominguez <migueldominguezroman01@gmail.com>
2023-01-16 10:15:07 +0100

    16/01/2023

--- routes/postUpdateTask.routes.js
+++ routes/postUpdateTask.routes.js
@@ -4,17 +4,22 @@ const task = require("../models/tasks")
 
 router.post("/updateTask", async(req, res) =>{
     const userData = await req.body
-
-    if((await task.find({id: userData.id})).length != 0){
-        //console.log(userData)
-        console.log(await task.collection.find({id_task: Number(userData.id_task)}))
-        await task.collection.updateOne({id_task: Number(userData.id_task)}, {$set:{
-            task: userData.task
-        }})
-        res.send("Ok")
-    }else{
-        res.send("Not ok")
-    }  
+    
+    task.collection.findOne({id_task: userData.id_task})
+    .then(res => query(res))
+    
+    async function query(result){ 
+        if(result != null){
+            await task.collection.updateOne({id_task: Number(userData.id_task)}, {$set:{
+                id_task: userData.id_task
+            }})
+            res.send("Ok")
+        }else{
+            res.send({status: "No se encontro ningún usuario asociado a ese identificador"})
+        }
+    }
+    
+    
 })
 
 
